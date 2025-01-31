98a51b6dbc4e7f7d369433ee2849b9dad3c2fe1e
@specktro <specktro@nonull.mx>
2025-01-30 20:06:41 -0600

    chore: Add yargs package to create and options to receive as execution parameters

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,3 @@
-import fs from 'fs'
+import {yarg} from "./config/plugins/yargs.plugin";
 
-let outputMessage = ''
-const base = 5
-const headerMessage = `
-==========================================
-            Tabla del ${base}
-==========================================\n
-`
-for (let i = 1; i <= 10; i++) {
-    outputMessage += `${base} x ${i} = ${base*i}\n`
-}
-
-outputMessage = headerMessage + outputMessage
-console.log(outputMessage)
-
-const outputPath = 'outputs'
-fs.mkdirSync(outputPath, { recursive: true })
-fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage)
+console.log(yarg)
