d01d1153c1990f45b71fa67a152d5df2bbe45788
@specktro <specktro@nonull.mx>
2025-01-31 19:00:49 -0600

    feat: Add yargs params to multiplication operation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
+import fs from 'fs'
 import {yarg} from "./config/plugins/yargs.plugin";
 
-console.log(yarg)
+let outputMessage = ''
+const base = yarg.b
+const limit = yarg.l
+const show = yarg.s
+const headerMessage = `
+==========================================
+            Tabla del ${base}
+==========================================\n
+`
+for (let i = 1; i <= limit; i++) {
+    outputMessage += `${base} x ${i} = ${base*i}\n`
+}
+
+outputMessage = headerMessage + outputMessage
+
+if (show) {
+    console.log(outputMessage)
+}
+
+const outputPath = 'outputs'
+fs.mkdirSync(outputPath, { recursive: true })
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage)
