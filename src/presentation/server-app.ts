d983438109d44cc5f8da258cc802e9b6443ddf1b
@specktro <specktro@nonull.mx>
2025-02-03 08:18:41 -0600

    feat: Add create-table use case to separate domain logic

---
+++ src/presentation/server-app.ts
@@ -0,0 +1,18 @@
+import {CreateTable} from "../domain/use-cases/create-table.use-case";
+
+interface RunOptions {
+    base: number
+    limit: number
+    showTable: boolean
+}
+
+export class ServerApp {
+    static run({base, limit, showTable}: RunOptions) {
+        const table = new CreateTable()
+            .execute({base, limit})
+
+        if (showTable) {
+            console.log(table)
+        }
+    }
+}
