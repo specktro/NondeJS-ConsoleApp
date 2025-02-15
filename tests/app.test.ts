51ec7b76d0d506cfa8955c879bcfe1a7e7cdb6ab
@specktro <specktro@nonull.mx>
2025-02-14 18:46:00 -0600

    feat: Add test cases for App.ts

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,19 @@
-describe('App test', () => {
-    test('Should be true', () => {
-        expect(true).toBeTruthy()
+import {ServerApp} from "../src/presentation/server-app";
+
+describe('App.ts test', () => {
+    test('Should call Server.run with values', async () => {
+        const serverRunMock = jest.fn()
+        ServerApp.run = serverRunMock
+        process.argv = ['node', 'app.ts', '-b', '10', '-l', '5', '-s', '-n', 'test-file', '-d', 'test-destination']
+
+        await import('../src/app')
+
+        expect(serverRunMock).toHaveBeenCalledWith({
+            base: 10,
+            limit: 5,
+            showTable: true,
+            fileName: 'test-file',
+            destination: 'test-destination',
+        })
     })
 })
